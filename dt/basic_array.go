61f16ed982ac3b9503866b3619fd8df44d065cd5
Vishnu Sankar <visank@microsoft.com>
2025-07-17 13:13:02 -0500

    bustbust

---
+++ dt/basic_array.go
@@ -0,0 +1,7 @@
+package basic_array
+
+var basicArray = []int{1, 2, 3, 4, 5}
+
+func GetBasicArray() []int {
+	return basicArray
+}
--- main.go
+++ main.go
@@ -1,2 +1,11 @@
-package github.com/vishnu-meera/ihavenoidea_1
+package main
 
+import "github.com/vishnu-meera/ihavenoidea_1/dt/basic_array"
+
+func main() {
+	arr := basic_array.GetBasicArray()
+
+	for _, value := range arr {
+		println(value)
+	}
+}
