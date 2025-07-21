6668a7415f999e13c19a4f790dcdbf64926a64f3
Vishnu Sankar <visank@microsoft.com>
2025-07-21 01:49:36 -0500

    linked list

---
+++ linkedList/linked_list.go
@@ -0,0 +1,132 @@
+package linkedList
+
+import (
+	"fmt"
+)
+type Node struct {
+	val int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (ll *LinkedList) Append(val int){
+	node:= &Node{val:val}
+
+	if ll.head == nil {
+		ll.head = node
+		return
+	}
+
+	curr := ll.head
+
+	for curr.next != nil {
+		curr = curr.next
+	}
+	curr.next = node
+}
+
+func (ll *LinkedList) Print(){
+	curr := ll.head
+
+	for curr != nil {
+		fmt.Printf("%d -> ", curr.val)
+		curr = curr.next
+	}
+
+	fmt.Println(nil)
+}
+
+func (ll *LinkedList) Remove(val int) {
+	if ll.head == nil {
+		return
+	}
+
+	if ll.head.val == val {
+		ll.head = ll.head.next
+		return
+	}
+
+	prev := ll.head
+	curr := ll.head.next 
+
+	for curr != nil {
+		if curr.val == val {
+			prev.next = curr.next
+			return
+		}
+		prev = curr
+		curr = curr.next
+	}
+}
+
+
+type LinkedListTail struct {
+	head *Node
+	tail *Node 
+}
+
+func (ll *LinkedListTail) Append(val int){
+	node:= &Node{val:val}
+
+	if ll.head == nil {
+		ll.head = node
+		ll.tail = node
+		return
+	}
+	ll.tail.next = node
+	ll.tail = node
+}
+
+func (ll *LinkedListTail) Prepend(val int){
+	node := &Node{val:val}
+	node.next = ll.head
+	ll.head = node
+
+	if ll.tail == nil {
+		ll.tail = node
+	}
+}
+
+func (ll *LinkedListTail) Print(){
+	curr := ll.head
+
+	for curr != nil {
+		fmt.Printf("%d -> ", curr.val)
+		curr = curr.next
+	}
+
+	fmt.Println(nil)
+}
+
+func (ll *LinkedListTail) Remove(val int) {
+	if ll.head == nil {
+		return
+	}
+
+	if ll.head.val == val {
+		ll.head = ll.head.next
+		if ll.head == nil {
+			ll.tail = nil
+		}
+		return
+	}
+
+	prev := ll.head
+	curr := ll.head.next 
+
+	for curr != nil {
+		if curr.val == val {
+			prev.next = curr.next
+			if curr == ll.tail {
+				ll.tail = prev
+			}
+			return
+		}
+		prev = curr
+		curr = curr.next
+	}
+}
+
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/vishnu-meera/ihavenoidea_1/arrays"
 	"github.com/vishnu-meera/ihavenoidea_1/stringss"
+	"github.com/vishnu-meera/ihavenoidea_1/linkedList"
 )
 
 func main() {
@@ -24,4 +25,37 @@ func main() {
     fmt.Println(stringss.Anagram("hello", "world"))       // false
     fmt.Println(stringss.Anagram("aab", "abb"))           // false
     fmt.Println(stringss.Anagram("💖😊", "😊💖"))         // true
+
+	ll := &linkedList.LinkedList{}
+
+    ll.Append(10)
+    ll.Append(20)
+    ll.Append(30)
+    ll.Print() // 10 -> 20 -> 30 -> nil
+
+    ll.Remove(20)
+    ll.Print() // 10 -> 30 -> nil
+
+	fmt.Println("---------------------------")
+
+
+	lt := &linkedList.LinkedListTail{}
+
+	lt.Append(10)
+	lt.Append(20)
+	lt.Append(30)
+	lt.Print() // 10 -> 20 -> 30 -> nil
+
+	lt.Prepend(5)
+	lt.Print() // 5 -> 10 -> 20 -> 30 -> nil
+
+	lt.Remove(30)
+	lt.Print() // 5 -> 10 -> 20 -> nil
+
+	lt.Remove(5)
+	lt.Print() // 10 -> 20 -> nil
+
+	lt.Remove(10)
+	lt.Remove(20)
+	lt.Print() // nil (head and tail should both be nil)
 }
